01e5d2feda5475b6b275ade7a2a6d019f2765056
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2024-12-17 15:13:43 +0100

    WEB: Updating SignUp file

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -3,7 +3,45 @@ import { Input } from "../../components/Input"
 import { Button } from "../../components/Button"
 import { ButtonText } from "../../components/ButtonText"
 
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { api } from '../../services/api';
+import { showToasts } from '../../utils/toasts';
+
+
 export function SignUp() {
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState();
+
+    const navigate = useNavigate();
+
+    async function handleSignUp() {
+        try {
+            const response = await api.post('/users', {
+                username: username,
+                email: email,
+                password: password,
+            });
+            
+            showToasts.success(response.data.message);
+            handleNavigate();
+
+
+        } catch (error) {
+            if(error.message) {
+                showToasts.error(error.response.data.message);
+            }
+        };
+        
+    };
+
+    function handleNavigate() {
+        navigate('/');
+    };
+
+    
     return (
         <Container>
                 <div>
@@ -15,18 +53,22 @@ export function SignUp() {
                     <Input 
                         title="Seu nome"
                         placeholder="Exemplo: Maria da Silva"
+                        onChange={e => setUsername(e.target.value)}
                     />
                     <Input 
                         title="Email"
                         placeholder="Exemplo: exemplo@exemplo.com.br"
+                        onChange={e => setEmail(e.target.value)}
                     />
                     <Input 
+                        type="password"
                         title="Senha"
                         placeholder="No mínimo 6 caracteres"
+                        onChange={e => setPassword(e.target.value)}
                     />
-                    <Button title="Criar conta" />
+                    <Button title="Criar conta" onClick={handleSignUp}/>
                     <ButtonText title="Já tenho uma conta" />
                 </Form>
         </Container>
     )
-}
+};
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,11 +4,13 @@ import { AuthRoutes } from "./auth.routes";
 
 import { UserRoutes } from "./user.routes";
 import { AdminRoutes } from "./admin.routes";
+import { Toaster } from "sonner";
 
 
 export function Routes() {
     return (
         <BrowserRouter future={{ v7_relativeSplatPath: true, v7_startTransition: true }}>
+            <Toaster />
             <AuthRoutes />
         </BrowserRouter>
     )
