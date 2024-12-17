2d2ecdbb4f02faf4405f52731a03b97483dcebec
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2024-12-17 16:53:22 +0100

    WEB: Updating signIn page

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -2,16 +2,35 @@ import { Container, Form } from "./styles"
 
 import { Input } from "../../components/Input"
 import { Button } from "../../components/Button"
-import { ButtonText } from "../../components/ButtonText"
 
 import { useNavigate } from "react-router-dom"
+import { useState } from "react"
+
+import { useAuth } from '../../hooks/auth';
+
+import { showToasts } from "../../utils/toasts";
 
 export function SignIn() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState();
+
+    const { signIn } = useAuth();
+
     const navigate = useNavigate();
 
     function handleClickToRegister() {
         navigate("/register");
-    }
+    };
+
+    function handleLogin() {
+        const emptyField = !email || !password;
+
+        if(emptyField) {
+            return showToasts.error('Preencha todos os campos!');
+        };
+
+        signIn(email, password);
+    };
     
     return (
         <Container>
@@ -24,16 +43,18 @@ export function SignIn() {
                     <Input 
                         title="Email"
                         placeholder="Exemplo: exemplo@exemplo.com.br"
+                        onChange={e => setEmail(e.target.value)}
                     />
                     <Input 
                         title="Senha"
                         placeholder="No mínimo 6 caracteres"
+                        onChange={e => setPassword(e.target.value)}
                     />
-                    <Button title="Entrar" />
+                    <Button title="Entrar" onClick={handleLogin}/>
                     <a onClick={handleClickToRegister} >
                         Criar uma conta
                     </a>
                 </Form>
         </Container>
     )
-}
+};
