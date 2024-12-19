5be26c7dfe3e5f6e4eda98f6bd428261c1ad43b3
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2024-12-19 09:45:37 +0100

    WEB: Updating index file in Routes folder

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,14 +4,29 @@ import { AuthRoutes } from "./auth.routes";
 
 import { UserRoutes } from "./user.routes";
 import { AdminRoutes } from "./admin.routes";
+
 import { Toaster } from "sonner";
+import { useAuth } from "../hooks/auth";
 
 
 export function Routes() {
+    const { userData } = useAuth();
+
+    function AccessRoute() {
+        switch (userData.role) {
+            case 'admin': 
+                return <AdminRoutes />
+            case 'user':
+                return <UserRoutes />
+            default:
+                return <UserRoutes />
+        }
+    };
+    
     return (
         <BrowserRouter future={{ v7_relativeSplatPath: true, v7_startTransition: true }}>
             <Toaster />
-            <AuthRoutes />
+            { userData ? <AccessRoute /> : <AuthRoutes /> }
         </BrowserRouter>
     )
 }
