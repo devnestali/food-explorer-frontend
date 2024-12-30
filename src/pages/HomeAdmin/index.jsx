a1dffe6931ee93666219649984ed2c3145c4726b
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2024-12-30 16:15:39 +0100

    WEB: Updating HomeAdmin by creating function to fetch dish data

--- src/pages/HomeAdmin/index.jsx
+++ src/pages/HomeAdmin/index.jsx
@@ -1,15 +1,38 @@
 import { Container, MainHeader, Meals } from "./styles";
 
-import { Header } from '../../components/Header'
-import { Wrapper } from '../../components/Wrapper'
-import { Meal } from '../../components/Meal'
-import { Footer } from '../../components/Footer'
+import { Header } from '../../components/Header';
+import { Wrapper } from '../../components/Wrapper';
+import { Meal } from '../../components/Meal';
+import { Footer } from '../../components/Footer';
 
-import mainImage from '../../assets/mainImage.png'
+import { api } from "../../services/api";
+import { showToasts } from "../../utils/toasts";
+
+import mainImage from '../../assets/mainImage.png';
 
 import { LuChevronRight, LuChevronLeft } from "react-icons/lu";
 
+import { useEffect, useState } from "react";
+
 export function HomeAdmin() {
+    const [dishData, setDishData] = useState([]);
+    
+    useEffect(() => {
+        async function fetchDishData() {
+            try {
+                const response = await api.get(`/dish`);
+                setDishData(response.data);
+            } catch (error) {
+                if(error.message) {
+                    showToasts.error(error.response.data.message);
+                } else {
+                    console.error(error);
+                }
+            };
+        };
+        
+        fetchDishData();
+    }, []);
     return (
         <Container>
             <Header toAdmin />
@@ -27,10 +50,16 @@ export function HomeAdmin() {
                     <h2>Refeições</h2>
                     <Meals>
                         <a href=""><LuChevronRight /></a>
-                        <Meal toAdmin />
-                        <Meal toAdmin />
-                        <Meal toAdmin />
-                        <Meal toAdmin />
+                        {
+                            dishData && dishData.map((item) => (
+                                <Meal 
+                                    key={String(item.id)}
+                                    data={item}
+                                    toAdmin 
+                                />
+
+                            ))
+                        }
                         <a href=""><LuChevronLeft /></a>
                     </Meals>
                     
